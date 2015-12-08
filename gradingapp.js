96e26f05614221bd70c00161a1c2d7b832d07d56
jorcel1 <jasonorcel1@gmail.com>
2015-12-08 22:04:47 +0000

    started grading app

--- gradingapp.js
+++ gradingapp.js
@@ -3,7 +3,16 @@ function giveGrade() {
   var grade = document.getElementById("grade").value;
 
   console.log("we're here!");
-  alert("hey!");
+  alert
+
+if (grade <= 100 && grade >= 90)
+alert("A") 
+
+if (grade <= 89 && grade >= 80)
+alert("B")
+
+if (grade <= 79 && grade >= 70)
+alert("C")
 
 //Input your code below:
 
